20f9ccb79d971b76468f9bd157819c84f9780890
chakane <39193557+chakane3@users.noreply.github.com>
2022-05-04 17:47:14 -0400

    Updates

---
+++ precache-manifest.1957791414044c17235849b715411e72.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b5a831cc54707f1f4207",
+    "url": "/chakane/static/css/main.8b8b94b8.chunk.css"
+  },
+  {
+    "revision": "b5a831cc54707f1f4207",
+    "url": "/chakane/static/js/main.b5a831cc.chunk.js"
+  },
+  {
+    "revision": "0fa871e1478991ad1ae8",
+    "url": "/chakane/static/js/1.0fa871e1.chunk.js"
+  },
+  {
+    "revision": "66d796d8280200ec2af9",
+    "url": "/chakane/static/js/runtime~main.66d796d8.js"
+  },
+  {
+    "revision": "07b72bc5fce5a6d4c4f6bf90fe7399e5",
+    "url": "/chakane/index.html"
+  }
+];

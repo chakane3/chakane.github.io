ff0f5d5d6d6d4bc3b3d8beed026464ad190820a3
chakane <39193557+chakane3@users.noreply.github.com>
2022-05-07 14:45:00 -0400

    hosted on domain

--- chakane/src/components/Projects.js
+++ chakane/src/components/Projects.js
@@ -4,10 +4,20 @@ import ReactDom from "react-dom"
 function Projects() {
     return (
         <div className="projects">
-            <p className="projects--cheapAmmo">Find Cheap Ammo</p>
-            <iframe className="projects--proj1" src="https://giphy.com/embed/cVOp9kRugipjSAkf1A" width="236" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p></p>
-            <iframe className="projects--proj2" src="https://giphy.com/embed/OMU8kYGKOWFIEYdosa" width="236" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p></p>
-            <div className="projects--proj3">Project 3</div>
+            <div className="projects--proj1">
+                <p>find cheap ammo</p>
+                <iframe src="https://giphy.com/embed/cVOp9kRugipjSAkf1A" width="236" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
+                {/* <p className="projects--description">This project came from an problem I stumbled on where I noticed there were mulltiple websites selling the same brands of ammunition at different prices.</p> */}
+
+                
+            </div >
+            
+            <div className="projects--proj2">
+                <p>This project </p>
+                <iframe src="https://giphy.com/embed/OMU8kYGKOWFIEYdosa" width="236" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
+            </div>
+
+            
         </div>
     )
 }
